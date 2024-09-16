f77f3cf3d7075ea88039f6f75def385f429ae75a
mridul-outscal <mridul.agrawal@outscal.com>
2024-09-16 13:56:03 +0530

    created Move struct and added list of moves in Pokemon Base class

--- Pokemon/Pokemon/Pokemon.hpp
+++ Pokemon/Pokemon/Pokemon.hpp
@@ -1,8 +1,11 @@
 #pragma once
 #include <string>
+#include <vector>
+using namespace std;
 
 namespace N_Pokemon {
-    
+
+    struct Move;
     enum class PokemonType;
     
     class Pokemon {
@@ -12,6 +15,7 @@ namespace N_Pokemon {
         int health; 
         int maxHealth; 
         int attackPower;
+        vector<Move> moves; // Store the list of moves
     
         Pokemon(); 
         Pokemon(std::string p_name, PokemonType p_type, int p_health, int p_attackPower);
@@ -21,5 +25,9 @@ namespace N_Pokemon {
         void heal();
         virtual void attack(Pokemon* target) = 0;
         void takeDamage(int damage);
+
+    protected:
+        // Base implementation for selecting and using a move
+        void selectAndUseMove(Pokemon* target); 
     };
 }
