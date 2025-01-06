a567a1626863e1620a6dc4a86103a3c7131f2d01
Viresh_Jadhav <168194192+vireshjadhav@users.noreply.github.com>
2025-01-07 01:44:26 +0530

    Entering Player Name
    Asking players name and adding it to the string and display message

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    string player_name;
 
+    cout << "Enter your name: ";
+    cin >> player_name;
+    cout << "Great Start " << player_name << ", Looks like you' ha've masterd the opening of every code adventure. Keep this up,\nand you'll be acoding master in no time" << endl;
     return 0;
 }
