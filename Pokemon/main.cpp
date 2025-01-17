eb8c44c568461a708b951c16e73e6426f92f0f81
egeso14 <egeyilmaz2023@u.northwestern.edu>
2025-01-16 22:01:41 -0600

    first changes to the main function

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
+using std::string;
 
 int main() {
 
-    return 0;
+    std::cout << "Welcome to the wonderous world of Pokemon, young trainer." << "\n"
+        << "By what name should I address you?" << "\n" << std::endl;
+    string trainerName;
+    std::cin >> trainerName;
+    std::cout << "Congratulations " << trainerName << ", now you are almost ready for adventure!\n" << std::endl;
 }
