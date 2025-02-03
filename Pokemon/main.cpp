25e35bddd241d76896a73be571d6434b666ecfbe
Tejas Vasishta G R <wasishta38@gmail.com>
2025-02-04 01:22:39 +0530

    Welcome Player

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int main() {
 
+    string name;
+
+    cout << "enter your name: ";
+    cin >> name;
+    cout << "Congrats " << name << "! Welcome to the game!\n";
     return 0;
 }
