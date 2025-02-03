89aad47d804538ddcadf3fcac31e65392a4110bd
Dhroov Sharma <mailmeatdhroovsharma@gmail.com>
2025-02-03 22:41:53 +0530

    Created main() funtion

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    string player_name;
+    
+    cout << "Enter your name: ";
+    cin >> player_name;
+
+    cout << "Great Start " << player_name << ", you are ready to start." << endl;
 
     return 0;
 }
