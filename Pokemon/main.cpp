047e08521ab589368f151645e0018bd793d5f404
GR-tejas <95667323+GR-tejas@users.noreply.github.com>
2025-02-04 12:26:21 +0530

    Switch case

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -4,30 +4,45 @@ using namespace std;
 int main() {
 
     string name;
+    string chosen_pokemon;
     int choice;
 
-    cout << "enter your name: ";
-    cin >> name;
-    cout << "Congrats " << name << "! Welcome to the game!\n";
+    cout << "Professor Oak: Hello there! Welcome to the world of Pokemon!\n";
+    cout << "Professor Oak: My name is Oak. People call me the Pokemon Professor!\n";
 
-    cout << "Welcome to the world of Pokémon! I am Professor Oak.\n";
-    cout << "You can choose one of the following Pokémon:\n";
-    cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
-    cout << "Which Pokémon would you like to choose? Enter the number: ";
+    cout << "Professor Oak: But enough about me. Let's talk about you!\n";
+
+    cout << "Professor Oak: First, tell me, what’s your name?\n";
+    cin >> name;
+    cout << "Professor Oak: Ah, " << name<< "! What a fantastic name!\n";
+    cout << "Professor Oak: You must be eager to start your adventure. But first, you will need a Pokemon of your own!\n";
+
+    cout << "Professor Oak: I have three Pokemon here with me. They are all quite feisty!\n";
+    cout << "Professor Oak: Choose wisely...\n";
+    cout << "1. Charmander - The fire type. A real hothead!\n";
+    cout << "2. Bulbasaur - The grass type. Calm and collected!\n";
+    cout << "3. Squirtle - The water type. Cool as a cucumber!\n";
 
     cin >> choice;
 
     if (choice == 1) {
+        chosen_pokemon = "Charmander";
         cout << "You chose Bulbasaur! A wise choice.\n";
     }
     else if (choice == 2) {
+        chosen_pokemon = "Bulbasaur";
         cout << "You chose Charmander! A fiery choice.\n";
     }
     else if (choice == 3) {
+        chosen_pokemon = "Squirtle";
         cout << "You chose Squirtle! A cool choice.\n";
     }
     else {
         cout << "Invalid choice. Please restart the game.\n";
     }
+
+    cout << "Professor Oak: " << chosen_pokemon << " and you, " << name << ", are going to be the best of friends!\n";
+    cout << "Professor Oak: Your journey begins now! Get ready to explore the vast world of Pokemon!\n";
+
     return 0;
 }
