28297693b3c2df01464186011bcb3f914f5fada3
GR-tejas <95667323+GR-tejas@users.noreply.github.com>
2025-02-06 12:25:14 +0530

    Pokemon choice and type header files

---
+++ Pokemon/PokemonType.hpp
@@ -0,0 +1,8 @@
+#pragma once
+enum class PokemonType {
+	FIRE,
+	GRASS,
+	WATER,
+	ELECTRIC,
+	NORMAL // Added for the default constructor
+};
--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <limits> // Include this header to use numeric_limits
 #include <string>
+#include"PokemonType.hpp"
+#include"PokemonChoice.hpp"
 using namespace std;
 
 // Function to clear the console
@@ -17,23 +19,6 @@ void clearConsole() {
 void waitForEnter() {
     cin.get(); // Wait for Enter key
 }
-
-// Define an enum for Pokemon choices
-enum class PokemonChoice {
-    CHARMANDER = 1,
-    BULBASAUR,
-    SQUIRTLE,
-    PIKACHU // Default choice
-};
-
-// Define an enum for Pokemon types
-enum class PokemonType {
-    FIRE,
-    GRASS,
-    WATER,
-    ELECTRIC,
-    NORMAL // Added for the default constructor
-};
 
 // Pokemon class definition
 class Pokemon {
@@ -305,4 +290,4 @@ int main() {
     gameLoop(player);
 
     return 0;
-#include "Header.hpp"
+}
